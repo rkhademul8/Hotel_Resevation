e9973affc1a6a96adc1a5e3b1c273ab7ed722562
rkhademul8 <75939920+rkhademul8@users.noreply.github.com>
2021-04-26 02:05:46 +0600

    user_signup_login

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage,name="homepage"),
+    path('about', views.aboutpage,name="aboutpage"),
+    path('contact', views.contactpage,name="contactpage"),
+    path('user', views.user_log_sign_page,name="userloginpage"),
+    path('user/signup', views.user_sign_up,name="usersignup"),
+    path('user/login', views.user_log_sign_page,name="userloginpage"),
+
 ]
--- main/views.py
+++ main/views.py
@@ -12,3 +12,73 @@ import datetime
 #homepage
 def homepage(request):
     return render(request, "index.html")
+
+
+def aboutpage(request):
+    return HttpResponse(render(request,'about.html'))
+
+#contact page
+def contactpage(request):
+    return HttpResponse(render(request,'contact.html'))
+
+
+
+
+
+#user sign up
+def user_sign_up(request):
+    if request.method =="POST":
+        user_name = request.POST['username']
+        
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 != password2:
+            messages.warning(request,"Password didn't matched")
+            return redirect('userloginpage')
+        
+        try:
+            if User.objects.all().get(username=user_name):
+                messages.warning(request,"Username Not Available")
+                return redirect('userloginpage')
+        except:
+            pass
+            
+
+        new_user = User.objects.create_user(username=user_name,password=password1)
+        new_user.is_superuser=False
+        new_user.is_staff=False
+        new_user.save()
+        messages.success(request,"Registration Successfull")
+        return redirect("userloginpage")
+    return HttpResponse('Access Denied')
+
+
+
+
+#user login and signup page
+def user_log_sign_page(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['pswd']
+
+        user = authenticate(username=email,password=password)
+        try:
+            if user.is_staff:
+                
+                messages.error(request,"Incorrect username or Password")
+                return redirect('staffloginpage')
+        except:
+            pass
+        
+        if user is not None:
+            login(request,user)
+            messages.success(request,"successful logged in")
+            print("Login successfull")
+            return redirect('homepage')
+        else:
+            messages.warning(request,"Incorrect username or password")
+            return redirect('userloginpage')
+
+    response = render(request,'user/userlogsign.html')
+    return HttpResponse(response)
