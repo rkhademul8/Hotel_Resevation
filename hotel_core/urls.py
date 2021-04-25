27263155eaa5457893f7a74625c08df44a2adee3
rkhademul8 <75939920+rkhademul8@users.noreply.github.com>
2021-04-25 19:52:16 +0600

    templates

--- hotel_core/urls.py
+++ hotel_core/urls.py
@@ -15,7 +15,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import main.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homepage,name="homepage"),
+    path('home', views.homepage,name="homepage"),
+    path('about', views.aboutpage,name="aboutpage"),
+    path('contact', views.contactpage,name="contactpage"),
+    path('user', views.user_log_sign_page,name="userloginpage"),
+    path('user/login', views.user_log_sign_page,name="userloginpage"),
+    path('user/bookings', views.user_bookings,name="dashboard"),
+    path('user/book-room', views.book_room_page,name="bookroompage"),
+    path('user/book-room/book', views.book_room,name="bookroom"),
+    path('user/signup', views.user_sign_up,name="usersignup"),
+    path('staff/', views.staff_log_sign_page,name="staffloginpage"),
+    path('staff/login', views.staff_log_sign_page,name="staffloginpage"),
+    path('staff/signup', views.staff_sign_up,name="staffsignup"),
+    path('logout', views.logoutuser,name="logout"),
+    path('staff/panel', views.panel,name="staffpanel"),
+    path('staff/allbookings', views.all_bookings,name="allbookigs"),
+    
+    path('staff/panel/add-new-location', views.add_new_location,name="addnewlocation"),
+    path('staff/panel/edit-room', views.edit_room),
+    path('staff/panel/add-new-room', views.add_new_room,name="addroom"),
+    path('staff/panel/edit-room/edit', views.edit_room),
+    path('staff/panel/view-room', views.view_room),
 ]
--- main/models.py
+++ main/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 
 class Hotels(models.Model):
     #h_id,h_name,owner ,location,rooms
-    name = models.CharField(max_length=30,default="krishna")
+    name = models.CharField(max_length=30,null=True)
     owner = models.CharField(max_length=20)
     location = models.CharField(max_length=50)
-    state = models.CharField(max_length=50,default="maharashtra")
-    country = models.CharField(max_length=50,default="india")
+    state = models.CharField(max_length=50,null=True)
+    country = models.CharField(max_length=50,null=True)
     def __str__(self):
         return self.name
 
